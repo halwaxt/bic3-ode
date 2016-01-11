57a31367c403431700d775ee66a16af656e6a969
Thomas Zeitinger <ic14b033@technikum-wien.at>
2016-01-11 22:43:29 +0100

    fixed comparing loop in Testclass

--- Katzenmemory/src/Katzenmemory/Testklasse.java
+++ Katzenmemory/src/Katzenmemory/Testklasse.java
@@ -29,15 +29,17 @@ public class Testklasse {
         System.out.println("Memorysize: " + memorysize);
         
         GameFactory gamefactory = new GameFactory();
+        System.out.println("New Memoryset:");
         gamefactory.getMemoryset().forEach(System.out::println);
         
         Gameplay gameplay = new Gameplay();
+        System.out.println("New Gameset:");
         gameplay.getGameset().forEach(System.out::println);
         
-        for (int j=0; j <= memorysize; j++) {
-            for (int k=0; k <= memorysize; k++) {
+        for (int j=0; j <= (memorysize*2)-1; j++) {
+            for (int k=0; k <= (memorysize*2)-1; k++) {
                 if (! ( (int)j == (int)k )) {
-                    if (gameplay.compareCards(j, k)) {
+                    if (gameplay.compareCards((int)j, (int)k)) {
                         System.out.println("Treffer bei j: " +j+ " und k: " +k);
                     }
                 }
